0dd5b89b6ad28196b13b77fa6167e128cf5ca2eb
b.m.ulyashev <b.m.ulyashev@mail.ru>
2022-09-29 20:47:56 +0300

    add service

---
+++ src/main/java/ru/hogwarts/school/service/FacultyService.java
@@ -0,0 +1,32 @@
+package ru.hogwarts.school.service;
+
+import org.springframework.stereotype.Service;
+import ru.hogwarts.school.model.Faculty;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
+public class FacultyService {
+    private Map<Long, Faculty> faculties = new HashMap<>();
+    private long countId = 0L;
+
+    public Faculty createFaculty(Faculty faculty) {
+        faculty.setId(++countId);
+        faculties.put(countId, faculty);
+        return faculty;
+    }
+
+    public Faculty findFaculty(long id) {
+        return faculties.get(id);
+    }
+
+    public Faculty editFaculty(Faculty faculty) {
+        faculties.put(faculty.getId(), faculty);
+        return faculty;
+    }
+
+    public Faculty deleteFaculty(long id) {
+        return faculties.remove(id);
+    }
+}
---
+++ src/main/java/ru/hogwarts/school/service/StudentService.java
@@ -0,0 +1,33 @@
+package ru.hogwarts.school.service;
+
+import org.springframework.stereotype.Service;
+import ru.hogwarts.school.model.Student;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
+public class StudentService {
+
+    private Map<Long, Student> students = new HashMap<>();
+    private long countId = 0L;
+
+    public Student createStudent(Student student) {
+        student.setId(++countId);
+        students.put(countId, student);
+        return student;
+    }
+
+    public Student findStudent(long id) {
+        return students.get(id);
+    }
+
+    public Student editStudent(Student student) {
+        students.put(student.getId(), student);
+        return student;
+    }
+
+    public Student deleteStudent(long id) {
+        return students.remove(id);
+    }
+}
