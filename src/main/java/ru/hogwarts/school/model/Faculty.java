0ba1510619badc4a43cbd199aeda0c45a200bfb0
b.m.ulyashev <b.m.ulyashev@mail.ru>
2022-10-06 21:22:40 +0300

    add ManyToOne and OneToMany

--- src/main/java/ru/hogwarts/school/model/Faculty.java
+++ src/main/java/ru/hogwarts/school/model/Faculty.java
@@ -3,7 +3,9 @@ package ru.hogwarts.school.model;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import java.util.Objects;
+import java.util.Set;
 
 @Entity
 public class Faculty {
@@ -14,6 +16,8 @@ public class Faculty {
     private String name;
     private String color;
 
+    @OneToMany(mappedBy = "faculty")
+    private Set<Student> students;
 
     public long getId() {
         return id;
